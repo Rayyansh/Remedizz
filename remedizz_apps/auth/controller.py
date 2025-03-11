c6c7296e02b56dee018624cb4ef650b32d6ef5de
Harshad <harshadkumbhar28@gmail.com>
2025-03-11 13:24:35 +0530

    trying for user auth

--- remedizz_apps/auth/controller.py
+++ remedizz_apps/auth/controller.py
@@ -1,82 +1,85 @@
+# from drf_spectacular.utils import extend_schema
+# from rest_framework.decorators import api_view
+# from rest_framework.request import Request
+# from rest_framework.response import Response
+#
+# from biller_apps.app_template.serializers.request.create import AppTemplateRequestSerializer
+# from biller_apps.app_template.serializers.request.delete import AppTemplateDeleteRequestSerializer
+# from biller_apps.app_template.serializers.request.delete_many import AppTemplateDeleteManySerializer
+# from biller_apps.app_template.serializers.request.update import AppTemplateUpdateRequestSerializer
+# from biller_apps.app_template.serializers.response.get_all import AppTemplateGetAllSerializer
+# from biller_apps.app_template.views import AppTemplateView
+# from biller_apps.common.serializer_validations import SerializerValidations
+# from biller_apps.common.serializers.request.get_all import GetAllSerializer
+# from biller_apps.common.serializers.request.search import SearchSerializer
+# from biller_apps.common.swagger import SwaggerPage
+#
+#
+# class AppTemplateViewController:
+#
+#     @extend_schema(
+#         description="Add an AppTemplate",
+#         request=AppTemplateRequestSerializer,
+#         responses=SwaggerPage.response(description=AppTemplateView().data_created)
+#     )
+#     @api_view(['POST'])
+#     @SerializerValidations(serializer=AppTemplateRequestSerializer,
+#                            exec_func='AppTemplateView().create_extract(request)').validate
+#     def create(request: Request) -> Response:
+#         return AppTemplateView().create_extract(params=request.params, token_payload=request.payload)
+
+
+
 from drf_spectacular.utils import extend_schema
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from biller_apps.app_template.serializers.request.create import AppTemplateRequestSerializer
-from biller_apps.app_template.serializers.request.delete import AppTemplateDeleteRequestSerializer
-from biller_apps.app_template.serializers.request.delete_many import AppTemplateDeleteManySerializer
-from biller_apps.app_template.serializers.request.update import AppTemplateUpdateRequestSerializer
-from biller_apps.app_template.serializers.response.get_all import AppTemplateGetAllSerializer
-from biller_apps.app_template.views import AppTemplateView
-from biller_apps.common.serializer_validations import SerializerValidations
-from biller_apps.common.serializers.request.get_all import GetAllSerializer
-from biller_apps.common.serializers.request.search import SearchSerializer
-from biller_apps.common.swagger import SwaggerPage
+from remedizz_apps.auth.serializers.request.create import UserSerializer, OTPSerializer
+from remedizz_apps.auth.views import UserView, OTPView
+from remedizz_apps.common.serializer_validations import SerializerValidations
+from remedizz_apps.common.swagger import SwaggerPage
 
 
-class AppTemplateViewController:
+class UserViewController:
 
     @extend_schema(
-        description="Add an AppTemplate",
-        request=AppTemplateRequestSerializer,
-        responses=SwaggerPage.response(description=AppTemplateView().data_created)
+        description="Create a new user",
+        request=UserSerializer,
+        responses=SwaggerPage.response(description=UserView().data_created)
     )
     @api_view(['POST'])
-    @SerializerValidations(serializer=AppTemplateRequestSerializer,
-                           exec_func='AppTemplateView().create_extract(request)').validate
+    @SerializerValidations(serializer=UserSerializer,
+                           exec_func='UserView().create_user(request)').validate
     def create(request: Request) -> Response:
-        return AppTemplateView().create_extract(params=request.params, token_payload=request.payload)
+        """Handles user registration"""
+        return UserView().create_user(params=request.data, token_payload=request.auth)
+
+
+class OTPViewController:
 
     @extend_schema(
-        description="Get all AppTemplate",
-        parameters=SwaggerPage.get_all_parameters(),
-        responses=SwaggerPage.response(response=AppTemplateGetAllSerializer)
+        description="Generate OTP for authentication",
+        request=OTPSerializer,
+        responses=SwaggerPage.response(description=OTPView().otp_generated)
     )
-    @api_view(['GET'])
-    @SerializerValidations(serializer=GetAllSerializer).validate
-    def get_all(request: Request) -> Response:
-        return AppTemplateView().get_all_extract(params=request.params, token_payload=request.payload)
+    @api_view(['POST'])
+    @SerializerValidations(serializer=OTPSerializer,
+                           exec_func='OTPView().generate_otp(request)').validate
+    def generate(request: Request) -> Response:
+        """Handles OTP generation"""
+        return OTPView().generate_otp(params=request.data, token_payload=request.auth)
 
     @extend_schema(
-        description="Delete a AppTemplate",
-        parameters=AppTemplateDeleteRequestSerializer.get_parameters(),
-        responses=SwaggerPage.response(description=AppTemplateView().data_delete)
+        description="Verify the OTP",
+        request=OTPSerializer,
+        responses=SwaggerPage.response(description=OTPView().otp_verified)
     )
-    @api_view(['DELETE'])
-    @SerializerValidations(serializer=AppTemplateDeleteRequestSerializer,
-                           exec_func='AppTemplateView().delete_extract(request)').validate
-    def delete(request: Request) -> Response:
-        return AppTemplateView().delete_extract(params=request.params, token_payload=request.payload)
+    @api_view(['POST'])
+    @SerializerValidations(serializer=OTPSerializer,
+                           exec_func='OTPView().verify_otp(request)').validate
+    def verify(request: Request) -> Response:
+        """Handles OTP verification"""
+        return OTPView().verify_otp(params=request.data, token_payload=request.auth)
 
-    @extend_schema(
-        description="Update an AppTemplate",
-        request=AppTemplateUpdateRequestSerializer,
-        responses=SwaggerPage.response(description=AppTemplateView().data_update)
-    )
-    @api_view(['PUT'])
-    @SerializerValidations(serializer=AppTemplateUpdateRequestSerializer,
-                           exec_func='AppTemplateView().update_extract(request)').validate
-    def update(request: Request) -> Response:
-        return AppTemplateView().update_extract(params=request.params, token_payload=request.payload)
 
-    @extend_schema(
-        description="Delete multiple app_templates",
-        request=AppTemplateDeleteManySerializer,
-        responses=SwaggerPage.response(description=AppTemplateView().data_delete)
-    )
-    @api_view(['PATCH'])
-    @SerializerValidations(serializer=AppTemplateDeleteManySerializer,
-                           exec_func='AppTemplateView().delete_many_extract(request)').validate
-    def delete_many(request: Request) -> Response:
-        return AppTemplateView().delete_many_extract(params=request.params, token_payload=request.payload)
-
-    @extend_schema(
-        description="Search a app_template ",
-        parameters=SwaggerPage.search_parameters(key_description="Search a app_template by name "),
-        responses=SwaggerPage.response(response=AppTemplateGetAllSerializer)
-    )
-    @api_view(['GET'])
-    @SerializerValidations(serializer=SearchSerializer).validate
-    def search(request: Request) -> Response:
-        return AppTemplateView().search_extract(params=request.params, token_payload=request.payload)
