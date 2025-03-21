28dab4bd77edc528dbefa01bc960e2b278931b1f
Rayyan Shaikh <rayyanshaikh@192.168.1.3>
2025-03-21 04:46:03 -0400

    Updated Allowed Hosts

--- remedizz/settings.py
+++ remedizz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-47+lsenh%r%j!x8^o5&vscel^&-g!6pg#n+18#b-q-fjw9bd&)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
