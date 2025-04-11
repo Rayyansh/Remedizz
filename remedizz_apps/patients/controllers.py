da783c3e489afbcfd9cc0d7e67ac3ed172ddee27
Rayyan Shaikh <rayyanshaikh@192.168.1.3>
2025-04-11 06:02:46 -0400

    Updated the Patient multiple logins

--- remedizz_apps/patients/controllers.py
+++ remedizz_apps/patients/controllers.py
@@ -6,8 +6,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from remedizz_apps.user.permissions import IsPatient
 
-from remedizz_apps.patients.views import PatientView
-from remedizz_apps.patients.serializers import PatientRequestSerializer, PatientResponseSerializer
+from remedizz_apps.patients.views import PatientView, ChildPatientView
+from remedizz_apps.patients.serializers.request.patient_create import PatientRequestSerializer
+from remedizz_apps.patients.serializers.response.patient_get_all import PatientResponseSerializer
+from remedizz_apps.patients.serializers.request.child_patient_create import ChildPatientRequestSerializer
+from remedizz_apps.patients.serializers.response.child_patient_get_all import ChildPatientResponseSerializer
 from remedizz_apps.common.swagger import SwaggerPage
 
 
@@ -38,3 +41,43 @@ class PatientController:
     @api_view(['DELETE'])
     def delete_patient(request: Request, patient_id) -> Response:
         return PatientView().delete(request, patient_id=patient_id)
+
+class ChildPatientController:
+
+    @extend_schema(
+        description="Create child patient profile",
+        request=ChildPatientRequestSerializer,
+        responses=SwaggerPage.response(description="Child created")
+    )
+    @api_view(['POST'])
+    @permission_classes([IsPatient])
+    def create_child(request):
+        return ChildPatientView().post(request)
+
+    @extend_schema(
+        description="Get all or one child profile",
+        responses=SwaggerPage.response(response=ChildPatientResponseSerializer)
+    )
+    @api_view(['GET'])
+    @permission_classes([IsPatient])
+    def get_children(request, child_id=None):
+        return ChildPatientView().get(request, child_id)
+
+    @extend_schema(
+        description="Update a child profile",
+        request=ChildPatientRequestSerializer,
+        responses=SwaggerPage.response(response=ChildPatientResponseSerializer)
+    )
+    @api_view(['PUT'])
+    @permission_classes([IsPatient])
+    def update_child(request, child_id):
+        return ChildPatientView().put(request, child_id)
+
+    @extend_schema(
+        description="Delete a child profile",
+        responses=SwaggerPage.response(description="Child deleted")
+    )
+    @api_view(['DELETE'])
+    @permission_classes([IsPatient])
+    def delete_child(request, child_id):
+        return ChildPatientView().delete(request, child_id)
--- remedizz_apps/patients/urls.py
+++ remedizz_apps/patients/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from remedizz_apps.patients.controllers import PatientController
+from remedizz_apps.patients.controllers import PatientController, ChildPatientController
 
 urlpatterns = [
     path('', PatientController.get_patient, name='get_all_patients'),
     path('<int:patient_id>/', PatientController.get_patient, name='get_patient'),
     path('update/<int:patient_id>/', PatientController.update_patient, name='update_patient'),
     path('delete/<int:patient_id>/', PatientController.delete_patient, name='delete_patient'),
+
+    path('child/', ChildPatientController.create_child),
+    path('child/all/', ChildPatientController.get_children),
+    path('child/<int:child_id>/', ChildPatientController.get_children),
+    path('child/update/<int:child_id>/', ChildPatientController.update_child),
+    path('child/delete/<int:child_id>/', ChildPatientController.delete_child),
 ]
 
 
