e35696b43ec3d7a08936f2c893984f9a74a58542
Rayyan Shaikh <rayyanshaikh@192.168.1.10>
2025-05-19 05:33:59 -0400

    Fixed the ChildResponse serializers

--- remedizz_apps/patients/serializers/response/child_patient_get_all.py
+++ remedizz_apps/patients/serializers/response/child_patient_get_all.py
@@ -3,7 +3,7 @@ from remedizz_apps.patients.models import ChildPatient
 
 
 class ChildPatientResponseSerializer(serializers.ModelSerializer):
-    parent_id = serializers.IntegerField(source="parent.id", read_only=True)
+    parent_id = serializers.IntegerField(source="parent.patient_id", read_only=True)
     parent_name = serializers.CharField(source="parent.name", read_only=True)
     gender = serializers.SlugRelatedField(read_only=True, slug_field='gender_name')
     city = serializers.SlugRelatedField(read_only=True, slug_field='city_name')
