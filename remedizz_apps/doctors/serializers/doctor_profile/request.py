b1132aa98e0de1cd973ddfc9bd8ded7d0f7900fe
Rayyan Shaikh <rayyanshaikh@192.168.1.4>
2025-05-26 04:59:42 -0400

    Fixed the Dr multiple Education and Work Experience

--- remedizz_apps/doctors/serializers/doctor_profile/request.py
+++ remedizz_apps/doctors/serializers/doctor_profile/request.py
@@ -28,47 +28,87 @@ class RegistrationCouncilRequestSerializer(serializers.ModelSerializer):
 
 
 class DoctorRequestSerializer(serializers.ModelSerializer):
-    education = EducationSerializer()
-    work_experience = WorkExperienceSerializer()
+    education = EducationSerializer(many=True)
+    work_experience = WorkExperienceSerializer(many=True)
 
     class Meta:
         model = Doctor
-        fields = [ 
-            "name","specialization", "gender", "city", "doctor_contact_number",
+        fields = [
+            "name", "specialization", "gender", "city", "doctor_contact_number",
             "doctor_email", "doctor_profile_picture", "education", "work_experience",
             "preferred_language", "terms_and_conditions_accepted",
             "registration_number", "registration_year", "registration_council"
         ]
 
-    def update(self, instance, validated_data):
-        education_data = validated_data.pop("education", None)
-        work_experience_data = validated_data.pop("work_experience", None)
+    def create(self, validated_data):
+        education_data = validated_data.pop("education", [])
+        work_experience_data = validated_data.pop("work_experience", [])
 
+        doctor = Doctor.objects.create(**validated_data)
+
+        for edu in education_data:
+            edu_instance = Education.objects.create(**edu)
+            doctor.education.add(edu_instance)
+
+        for work in work_experience_data:
+            work_instance = WorkExperience.objects.create(**work)
+            doctor.work_experience.add(work_instance)
+
+        return doctor
+
+    def update(self, instance, validated_data):
+        education_data = validated_data.pop("education", [])
+        work_experience_data = validated_data.pop("work_experience", [])
+
+        # === Update Doctor Fields ===
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
 
-        # Education
-        if education_data:
-            if instance.education:
-                for attr, value in education_data.items():
-                    setattr(instance.education, attr, value)
-                instance.education.save()
-            else:
-                education = Education.objects.create(**education_data)
-                instance.education = education
-                instance.save()
+        # === Update Education ===
+        existing_edu_ids = [edu.id for edu in instance.education.all()]
+        incoming_edu_ids = []
 
-        # Work Experience
-        if work_experience_data:
-            if instance.work_experience:
-                for attr, value in work_experience_data.items():
-                    setattr(instance.work_experience, attr, value)
-                instance.work_experience.save()
+        for edu in education_data:
+            edu_id = edu.get("id")
+            if edu_id and Education.objects.filter(id=edu_id).exists():
+                edu_instance = Education.objects.get(id=edu_id)
+                for attr, value in edu.items():
+                    setattr(edu_instance, attr, value)
+                edu_instance.save()
             else:
-                work_experience = WorkExperience.objects.create(**work_experience_data)
-                instance.work_experience = work_experience
-                instance.save()
+                edu_instance = Education.objects.create(**edu)
+                instance.education.add(edu_instance)
+
+            incoming_edu_ids.append(edu_instance.id)
+
+        # Delete removed educations
+        for edu in instance.education.all():
+            if edu.id not in incoming_edu_ids:
+                instance.education.remove(edu)
+                edu.delete()
+
+        # === Update Work Experience ===
+        existing_work_ids = [work.id for work in instance.work_experience.all()]
+        incoming_work_ids = []
+
+        for work in work_experience_data:
+            work_id = work.get("id")
+            if work_id and WorkExperience.objects.filter(id=work_id).exists():
+                work_instance = WorkExperience.objects.get(id=work_id)
+                for attr, value in work.items():
+                    setattr(work_instance, attr, value)
+                work_instance.save()
+            else:
+                work_instance = WorkExperience.objects.create(**work)
+                instance.work_experience.add(work_instance)
+
+            incoming_work_ids.append(work_instance.id)
+
+        # Delete removed work experiences
+        for work in instance.work_experience.all():
+            if work.id not in incoming_work_ids:
+                instance.work_experience.remove(work)
+                work.delete()
 
         return instance
-    
