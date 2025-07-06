776bd50fa021b18f5a91b1c46d79a3e4108cc2cc
Rayyan Shaikh <rayyanshaikh@Rayyans-Mac-mini.local>
2025-07-06 02:41:11 -0400

    Update the UPI ID for clinic payment

--- remedizz_apps/clinics/models.py
+++ remedizz_apps/clinics/models.py
@@ -142,7 +142,7 @@ class DigitalClinicPaymentInformation(models.Model):
     digital_clinic_bank_account_number = models.CharField(max_length=20)
     digital_clinic_bank_account_name = models.CharField(max_length=20, null=True)
     ifsc_code = models.CharField(max_length=11)
-    uoi_id = models.CharField(max_length=10)
+    uoi_id = models.CharField(max_length=30)
 
     def __str__(self):
         return self.digital_clinic_id.name
